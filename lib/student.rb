321c158f063234b087874afba1ed435cdf8f457d
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-07-07 20:35:31 -0400

    commit tree ba9bba182546ee69d3d880ba74c0f48ec98a562a

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,26 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
-  end
+    students_array.each do |student|
+     self.new(student)
+    end#each ender
+  end#method ender
 
   def add_student_attributes(attributes_hash)
-    
+    #binding.pry
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def self.all
-    
+    @@all
   end
+
+
+
 end
 
